d8259ba112f0c65f540310b889f3ea086420aad0
tyreke-mclean <tyrekemclean@gmail.com>
2020-11-01 23:58:02 -0500

    Some colour,picture, java, layout updates

--- scripts.js
+++ scripts.js
@@ -1,20 +1,20 @@
 /* Add your JavaScript to this file */
 document.addEventListener('DOMContentLoaded', function(){
     var result = document.getElementsByClassName("message")[0];
-    var  subscribe = document.getElementsByTagName("button")[0];
-    var emailAdd = document.getElementsByName("email");
+    var  button = document.getElementsByTagName("button")[0];
+    var email = document.getElementById("email");
     
   
-    subscribe.addEventListener('click', (e) =>{
-        e.preventDefault();
-        if (emailAdd.value === "" || emailAdd.value === null){
-             result.innerHTML=` Thank you! Your email address ${emailAdd.value} has been added to our mailing list!`;
-          
-            }
+    button.addEventListener('click', (event) =>{
+        event.preventDefault();
+        if (email.value === '' || email.value === null||email.value===""){
+            result.innerHTML= 'Please enter a valid email address.';
+            
+        }
         else{
-             result.innerHTML="Please enter a valid email address.";
-        
+            result.innerHTML = ` Thank you! Your email address, ${email.value}, has been added to our mailing list!`;
+            
          }
     });
      
- });
+ }, false);
